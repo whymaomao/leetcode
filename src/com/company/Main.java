7202431bfe428fb23741a93f5c313006188d5c03
Yue Hu <yue.hu@citrix.com>
2018-11-20 09:27:50 +0800

    No13_romanToInt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,11 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        No7_ReverseInterger solution = new No7_ReverseInterger();
-        System.out.println(solution.reverse(123));
+        No13_romanToInt solution = new No13_romanToInt();
+        System.out.println("III is " + solution.romanToInt("III"));
+        System.out.println("IV is " + solution.romanToInt("IV"));
+        System.out.println("IX is " + solution.romanToInt("IX"));
+        System.out.println("LVIII is " + solution.romanToInt("LVIII"));
+        System.out.println("MCMXCIV is " + solution.romanToInt("MCMXCIV"));
     }
 }
--- src/com/company/No13_romanToInt.java
+++ src/com/company/No13_romanToInt.java
@@ -1,4 +1,8 @@
 package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Symbol       Value
 I             1
@@ -9,8 +13,30 @@ C             100
 D             500
 M             1000
  */
-public class NO13_romanToInt {
+public class No13_romanToInt {
     public int romanToInt(String s) {
+        Map<Character,Integer> hashMap = new HashMap<Character,Integer>();
 
+        hashMap.put('I',1);
+        hashMap.put('V',5);
+        hashMap.put('X',10);
+        hashMap.put('L',50);
+        hashMap.put('C',100);
+        hashMap.put('D',500);
+        hashMap.put('M',1000);
+
+
+        int result = 0;
+        for(int i = 0; i < s.length(); i ++)
+        {
+            if( (i > 0) &&
+                    (hashMap.get(s.charAt(i-1)) < hashMap.get(s.charAt(i))))
+            {
+                result -= (2*hashMap.get(s.charAt(i -1)));
+            }
+            result += hashMap.get(s.charAt(i));
+
+        }
+        return result;
     }
 }
