e15c8f76936d4aa0ec101c4da3e9f3e9400f57d1
Yue Hu <yue.hu@citrix.com>
2019-04-09 09:26:27 +0800

    No1021

--- src/com/company/No1021_removeOuterParentheses.java
+++ src/com/company/No1021_removeOuterParentheses.java
@@ -1,4 +1,39 @@
 package com.company;
 
+import java.util.Stack;
+
 public class No1021_removeOuterParentheses {
+
+    public String removeOuterParentheses(String S) {
+        //check S can be removed outer parentheses
+        // Use stack
+
+        Stack<Character> stack = new Stack<>();
+        String result = "";
+        for(int i = 0; i < S.length(); i ++)
+        {
+            if(stack.isEmpty() && S.charAt(i) == '(')
+            {
+                stack.push('(');
+                continue;
+            }
+            if(stack.size() == 1 && S.charAt(i) == ')')
+            {
+                stack.pop();
+                continue;
+            }
+            if(S.charAt(i) == '(')
+            {
+                stack.push(S.charAt(i));
+            }
+            if(S.charAt(i) == ')')
+            {
+                stack.pop();
+            }
+            result += S.charAt(i);
+        }
+        return result;
+
+
+    }
 }
